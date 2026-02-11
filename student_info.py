af23762b59ffe277014964ad196b53c50ff3efd7
kiranswamy11-creator <kkirannswamy11@gmail.com>
2026-02-11 18:45:48 +0530

    Add files via upload

---
+++ student_info.py
@@ -0,0 +1,20 @@
+# Simple Student Details Program
+
+# Taking input from the user using input() function
+# We store these values in variables
+name = input("Enter Student Name: ")
+roll_number = input("Enter Roll Number: ")
+section = input("Enter Section: ")
+semester = input("Enter Semester: ")
+college = input("Enter College Name: ")
+
+# Displaying the stored details using print() statements
+print("\n-------------------------")
+print("    STUDENT DETAILS      ")
+print("-------------------------")
+print("Name        :", name)
+print("Roll Number :", roll_number)
+print("Section     :", section)
+print("Semester    :", semester)
+print("College     :", college)
+print("-------------------------")
